80a2219782deadbaa4fb840afa7921e09b14142e
adamwroo <adamwroo@gmail.com>
2022-02-27 19:51:49 +0100

    navigation added; about (in progress) added

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
+	import { Router, Link, Route } from "svelte-routing";
 	import { onMount } from 'svelte';
 	import { mortgageBase, alternatives, showAlternatives } from './stores';
 	import { getMortgage } from './calculations';
+	import About from './components/About.svelte';
+	import Icon from './components/Icon.svelte';
 	import MortgageBase from './components/MortgageBase.svelte';
 	import MortgageCost from './components/MortgageCost.svelte';
 	import Alternatives from './components/Alternatives.svelte';
@@ -16,26 +19,64 @@
 	$: mortgage = getMortgage($mortgageBase);
 </script>
 
-<main class="with-sidebar">
-	<div class="sidebar">
-		<div class="sticky">
-			<MortgageBase bind:mortgageBase={$mortgageBase} />
-			<MortgageCost {mortgage} />
-			<AlternativeScheduleSwitch bind:showAlternatives={$showAlternatives} />
-		</div>
+<Router>
+	<nav>
+		<Link to="/">
+			<div class="nav-icon">
+				<Icon />
+			</div>
+		</Link>
+		<Link to="about">
+			<span class="nav-text">Informacje</span>
+		</Link>
+	</nav>
+	<div>
+		<Route path="/">
+			<main class="with-sidebar">
+				<div class="sidebar">
+					<div class="sticky">
+						<MortgageBase bind:mortgageBase={$mortgageBase} />
+						<MortgageCost {mortgage} />
+						<AlternativeScheduleSwitch bind:showAlternatives={$showAlternatives} />
+					</div>
+				</div>
+				<div class="not-sidebar">
+					{#if $showAlternatives}
+					<Alternatives bind:alternatives={$alternatives} {mortgage} />
+					{:else}
+					<Schedule {mortgage} />
+					{/if}
+				</div>
+			</main>
+		</Route>
+		<Route path="about" component="{About}" />
 	</div>
-	<div class="not-sidebar">
-		{#if $showAlternatives}
-			<Alternatives bind:alternatives={$alternatives} {mortgage} />
-		{:else}
-			<Schedule {mortgage} />
-		{/if}
-	</div>
-</main>
+</Router>
 
 <style>
 	* {
 		--main-padding: 1em;
+		--nav-height: 2.5rem;
+		--nav-padding: 0.5rem;
+	}
+
+	nav {
+		display: flex;
+		height: var(--nav-height);
+		justify-content: left;
+		align-items: center;
+		gap: 1rem;
+		padding: var(--nav-padding);
+		border-bottom: 1px solid #ccc;
+	}
+
+	.nav-icon {
+		width: calc(var(--nav-height) - calc(var(--nav-padding) * 2));
+		height: calc(var(--nav-height) - calc(var(--nav-padding) * 2));
+	}
+
+	.nav-text {
+		font-size: 1.25rem;
 	}
 
 	main {
---
+++ src/components/About.svelte
@@ -0,0 +1,1 @@
+<h1>Informacje</h1>
