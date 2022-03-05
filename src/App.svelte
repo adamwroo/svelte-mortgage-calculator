ae6a2d7b46bea070b81565d74ca2a6e734ca5735
adamwroo <adamwroo@gmail.com>
2022-03-05 17:47:48 +0100

    Revert "navigation added; about (in progress) added"
    This reverts commit 80a2219782deadbaa4fb840afa7921e09b14142e.

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,7 @@
 <script>
-	import { Router, Link, Route } from "svelte-routing";
 	import { onMount } from 'svelte';
 	import { mortgageBase, alternatives, showAlternatives } from './stores';
 	import { getMortgage } from './calculations';
-	import About from './components/About.svelte';
-	import Icon from './components/Icon.svelte';
 	import MortgageBase from './components/MortgageBase.svelte';
 	import MortgageCost from './components/MortgageCost.svelte';
 	import Alternatives from './components/Alternatives.svelte';
@@ -19,64 +16,26 @@
 	$: mortgage = getMortgage($mortgageBase);
 </script>
 
-<Router>
-	<nav>
-		<Link to="/">
-			<div class="nav-icon">
-				<Icon />
-			</div>
-		</Link>
-		<Link to="about">
-			<span class="nav-text">Informacje</span>
-		</Link>
-	</nav>
-	<div>
-		<Route path="/">
-			<main class="with-sidebar">
-				<div class="sidebar">
-					<div class="sticky">
-						<MortgageBase bind:mortgageBase={$mortgageBase} />
-						<MortgageCost {mortgage} />
-						<AlternativeScheduleSwitch bind:showAlternatives={$showAlternatives} />
-					</div>
-				</div>
-				<div class="not-sidebar">
-					{#if $showAlternatives}
-					<Alternatives bind:alternatives={$alternatives} {mortgage} />
-					{:else}
-					<Schedule {mortgage} />
-					{/if}
-				</div>
-			</main>
-		</Route>
-		<Route path="about" component="{About}" />
+<main class="with-sidebar">
+	<div class="sidebar">
+		<div class="sticky">
+			<MortgageBase bind:mortgageBase={$mortgageBase} />
+			<MortgageCost {mortgage} />
+			<AlternativeScheduleSwitch bind:showAlternatives={$showAlternatives} />
+		</div>
 	</div>
-</Router>
+	<div class="not-sidebar">
+		{#if $showAlternatives}
+			<Alternatives bind:alternatives={$alternatives} {mortgage} />
+		{:else}
+			<Schedule {mortgage} />
+		{/if}
+	</div>
+</main>
 
 <style>
 	* {
 		--main-padding: 1em;
-		--nav-height: 2.5rem;
-		--nav-padding: 0.5rem;
-	}
-
-	nav {
-		display: flex;
-		height: var(--nav-height);
-		justify-content: left;
-		align-items: center;
-		gap: 1rem;
-		padding: var(--nav-padding);
-		border-bottom: 1px solid #ccc;
-	}
-
-	.nav-icon {
-		width: calc(var(--nav-height) - calc(var(--nav-padding) * 2));
-		height: calc(var(--nav-height) - calc(var(--nav-padding) * 2));
-	}
-
-	.nav-text {
-		font-size: 1.25rem;
 	}
 
 	main {
--- src/components/About.svelte
+++
@@ -1,1 +0,0 @@
-<h1>Informacje</h1>
